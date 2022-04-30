a6194912083a681e7b55012f443e3c861ecfc9f1
Bartek Fil <bart@o2.pl>
2022-04-30 18:25:37 +0200

    first com

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComponent from "./components/QuizComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+       <QuizComponent/>
+    </div>
+  );
+}
+
+export default App;
